51fb786e37a727aabfd6e35a94e7d6b622d3cd57
weiwi01 <weiwi01@ca.com>
2015-12-21 23:55:09 +0100

    2nd Wave -- starting to get it.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,79 +1,80 @@
-var work = {
-  "jobs": [
-	{
-	  "employer": "CA Technologies",
-	  "title": "Scrum Master",
-	  "location": "Prague, Czech Republic",
-	  "dates": "March 2015 - Present",
-	  "description": "Working as a facilitator and organizer of a crossfunctional Agile scrum team."
-    },
-    {
-      "employer": "CA Technologies",
-	  "title": "Sr. Technical Writer",
-	  "location": "Prague, Czech Republic",
-	  "dates": "July 2014 - Present",
-	  "description": "Creating world class product documentation for a SaaS Operating Platform."
-    },
-    {
-      "employer": "CA Technologies",
-	  "title": "Technical Writer",
-	  "location": "Prague, Czech Republic",
-	  "dates": "September 2011 - June 2014",
-	  "description": "Creating world class product documentation for a SaaS Operating Platform."
-	},
-	{
-      "employer": "Pokorny, Wagner & spol.",
-	  "title": "Legal Proofreader",
-	  "location": "Prague, Czech Republic",
-	  "dates": "September 2008 - July 2011",
-	  "description": "Proofreader of legal documentation presented before the European Commsion."
-	},
-	{
-	  "employer": "MEL Spracheninstitut",
-	  "title": "Business and Technical English Instructor",
-	  "location": "Nuremberg, Germany",
-	  "dates": "September 2006 - June 2008",
-	  "description": "Teacher of English in various companies and schools in the region of northern Baveria."	
-	}
-  ]
+var bio = {
+  "name": "Bill Weidenborner",
+  "role": "Scrum Master",
+  "contacts": {
+    "mobile": "+420 775 893 620",
+    "email": "warnerwilliam@gmail.com",
+    "github": "BillWW",
+    "twitter": "@Bill_W_Prague",
+    "location": "Prague"
+  },
+  "welcomeMessage": "Positive disruption, focused innovation, concrete results",
+  "skills": [
+  "Writing", "Innovating", "Facilitating"
+  ],
+  "biopic": "images/Me2.jpg"
 }
 
-var bio = {
-	"name": "Bill Weidenborner",
-	"role": "Scrum Master",
-	"welcomMessage": "The unexamined life is not worth living",
-	"contacts": {
-	  "email": "warnerwilliam@gmail.com",
-	  "mobile": "775 893 620",
-	  "github": "BillWW",
-	  "twitter": "@Bill_W_Prague",
-	  "location": "Prague"
-	},
-	"skills": [
-	"Writing", "Talking", "Thinking"
-	]
+var work = {
+  "jobs": [
+   {
+    "employer": "CA Technologies",
+    "title": "Scrum Master",
+    "location": "Prague, Czech Republic",
+    "dates": "March 2015 - Present",
+    "description": "Working as a facilitator and organizer of a crossfunctional Agile scrum team."
+    },
+    {
+    "employer": "CA Technologies",
+    "title": "Sr. Technical Writer",
+    "location": "Prague, Czech Republic",
+    "dates": "July 2014 - Present",
+    "description": "Creating world class product documentation for a SaaS Operating Platform."
+    },
+    {
+    "employer": "CA Technologies",
+    "title": "Technical Writer",
+    "location": "Prague, Czech Republic",
+    "dates": "September 2011 - June 2014",
+    "description": "Creating world class product documentation for a SaaS Operating Platform."
+    },
+    {
+    "employer": "Pokorny, Wagner & spol.",
+    "title": "Legal Proofreader",
+    "location": "Prague, Czech Republic",
+    "dates": "September 2008 - July 2011",
+    "description": "Proofreader of legal documentation presented before the European Commsion."
+    },
+    {
+    "employer": "MEL Spracheninstitut",
+    "title": "Business and Technical English Instructor",
+    "location": "Nuremberg, Germany",
+    "dates": "September 2006 - June 2008",
+    "description": "Teacher of English in various companies and schools in the region of northern Baveria." 
+    }
+  ]
 }
 
 var education = {
   "schools": [
    {
      "name": "Hunter College",
-     "city": "New York, NY",
+     "location": "New York, NY",
      "degree": "Bachelor of Arts",
      "major": "English Literature",
-     "dates": "January 1998 - June 2001"
+     "dates": 2001
    },
    {
-  	 "name": "Montclair High School",
-  	 "city": "Montclair, NJ",
-  	 "degree": "diploma",
-  	 "dates": "September 1992 - June 1996"
+     "name": "Montclair High School",
+     "location": "Montclair, NJ",
+     "degree": "diploma",
+     "dates": 1996
    },
    {
-  	 "name": "Next Level Institue",
-  	 "city": "Prague, Czech Republic",
-  	 "degree": "TEFL Certificate",
-  	 "dates": "January 2002"
+     "name": "Next Level Institue",
+     "location": "Prague, Czech Republic",
+     "degree": "TEFL Certificate",
+     "dates": 2002
    }
   ],
   "onlineCourses": [
@@ -90,8 +91,8 @@ var education = {
       "url": "https://www.udacity.com/course/viewer#!/c-ud893-nd"
     },
     {
-   	  "title": "JavaScript Basics",
-   	  "school": "Udacity",
+      "title": "JavaScript Basics",
+      "school": "Udacity",
       "dates": 2015,
       "url": "https://www.udacity.com/course/viewer#!/c-ud804-nd"
    }
@@ -100,13 +101,61 @@ var education = {
 
 var projects = {
   "projects": [
-	{
-	   "title": "Portfolio Site",
-	   "dates": "November 2015",
-	   "description": "Building of a personalized portfolio in HTML/CSS site using a grid framework."
-	}
+  {
+     "title": "Portfolio Site",
+     "dates": "November 2015",
+     "description": "Building of a personalized portfolio in HTML/CSS site using a grid framework."
+  }
   ]
-}	
+} 
+
+
+bio.display = function() {
+
+
+  var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+  $("#header").prepend(formattedRole);
+
+  var formattedName = HTMLheaderName.replace("%data%", bio.name);
+  $("#header").prepend(formattedName);
+
+  var formattedMobile = HTMLmobile.replace("%data%", bio.contacts.mobile);
+  $("#header").append(formattedMobile);
+
+  var formattedEmail = HTMLemail.replace("%data%", bio.contacts.email);
+  $("#header").append(formattedEmail);
+
+  var formattedGithub = HTMLgithub.replace("%data%", bio.contacts.github);
+  $("#header").append(formattedGithub);
+
+  var formattedTwitter = HTMLtwitter.replace("%data%", bio.contacts.twitter);
+  $("#header").append(formattedTwitter);
+
+  var formattedLocation = HTMLlocation.replace("%data%", bio.contacts.location);
+  $("#header").append(formattedLocation);
+
+  var formattedPic = HTMLbioPic.replace("%data%", bio.biopic);
+  $("#header").append(formattedPic);
+
+  var formattedMessage = HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage);
+  $("#header").append(formattedMessage);
+
+  $("#header").append(HTMLskillsStart);
+
+  var formattedSkill = HTMLskills.replace("%data%", bio.skills[0])
+  $("#skills").append(formattedSkill);
+  formattedSkill = HTMLskills.replace("%data%", bio.skills[1]);
+  $("#skills").append(formattedSkill);
+  formattedSkill = HTMLskills.replace("%data%", bio.skills[2]);
+  $("#skills").append(formattedSkill);
+
+  }
+
+bio.display();
+
+
+
+
 
 projects.display = function() {
 
@@ -137,19 +186,19 @@ education.display = function() {
   var formattedSchool = HTMLschoolName.replace("%data%", education.schools[school].name);
   $(".education-entry:last").append(formattedSchool);
 
-  var formattedSchoolCity = HTMLschoolLocation.replace("%data%", education.schools[school].city);
+  var formattedSchoolDegree = HTMLschoolDegree.replace("%data%", education.schools[school].degree);
+  $(".education-entry:last").append(formattedSchoolDegree);
+
+  var formattedSchoolCity = HTMLschoolLocation.replace("%data%", education.schools[school].location);
   $(".education-entry:last").append(formattedSchoolCity);
 
   var formattedSchoolDates = HTMLschoolDates.replace("%data%", education.schools[school].dates);
   $(".education-entry:last").append(formattedSchoolDates);
 
-  var formattedSchoolDegree = HTMLschoolDegree.replace("%data%", education.schools[school].degree);
-  $(".education-entry:last").append(formattedSchoolDegree);
-
-  if (education.schools[school].major > 0) {
+  if (education.schools.major > 0) {
     for (major in education.schools[schoo].major) {
-  	  var formattedSchoolMajor = HTMLschoolMajor.replace("%data%", education.schools[school].major);
-      $(".education-entry:last").append(formattedSchoolMajor);
+      var formattedSchoolMajor = HTMLschoolMajor.replace("%data%", education.schools[school].major);
+      $(".education-entry:last").prepend(formattedSchoolMajor);
    }
   }
  }
@@ -158,48 +207,42 @@ education.display = function() {
 
 education.display();
 
+$("#mapDiv").append(googleMap);
 
 function displayWork() {
 
 for (job in work.jobs) {
-	$("#workExperience").append(HTMLworkStart);
+  $("#workExperience").append(HTMLworkStart);
 
-	var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
+  var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
 
-	var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
+  var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
 
-	var formattedEmployerTitle = formattedEmployer + formattedTitle;
-	$(".work-entry:last").append(formattedEmployerTitle);
+  var formattedEmployerTitle = formattedEmployer + formattedTitle;
+  $(".work-entry:last").append(formattedEmployerTitle);
 
-	var formattedLocation = HTMLworkLocation.replace("%data%", work.jobs[job].location);
-	$(".work-entry:last").append(formattedLocation);
+  var formattedLocation = HTMLworkLocation.replace("%data%", work.jobs[job].location);
+  $(".work-entry:last").append(formattedLocation);
 
-	var formattedDates = HTMLworkDates.replace("%data%", work.jobs[job].dates);
-	$(".work-entry:last").append(formattedDates);
+  var formattedDates = HTMLworkDates.replace("%data%", work.jobs[job].dates);
+  $(".work-entry:last").append(formattedDates);
 
-	var formattedDescription = HTMLworkDescription.replace("%data%", work.jobs[job].description);
-	$(".work-entry:last").append(formattedDescription);
+  var formattedDescription = HTMLworkDescription.replace("%data%", work.jobs[job].description);
+  $(".work-entry:last").append(formattedDescription);
 
-	}
+  }
 }
 
 displayWork();
 
 
-if(bio.skills.length > 0) {
+//if(bio.skills.length > 0) {
 
-	$("#header").append(HTMLskillsStart);
-
-	var formattedSkill = HTMLskills.replace("%data%", bio.skills[0])
-	$("#skills").append(formattedSkill);
-	formattedSkill = HTMLskills.replace("%data%", bio.skills[1]);
-	$("#skills").append(formattedSkill);
-	formattedSkill = HTMLskills.replace("%data%", bio.skills[2]);
-	$("#skills").append(formattedSkill);
-}
+  
+//}
 
 function inName(name) {
-	console.log(name);
+  console.log(name);
     var name = bio.name.split(" ");
     name[1] = name[1].toUpperCase();
     name[0] = name[0].slice(0,1).toUpperCase() + name[0].slice(1).toLowerCase();
@@ -211,15 +254,12 @@ function inName(name) {
   
    }
 
-$("#main").append(internationalizeButton);
+//$("#main").append(internationalizeButton);
 
 
-//displayWork();
 
-//$(document).click(function(loc) {
-	//var x = loc.pageX;
-//	var y = loc.pageY;
-	//logClicks(x,y);});
+
+
 
 //work.company = "CA Technologies";
 //work.position = "Scrum Master";
@@ -236,28 +276,5 @@ $("#main").append(internationalizeButton);
 
 
 
-var formattedName = HTMLheaderName.replace("%data%", bio.name);
-
-var formattedRole = HTMLheaderRole.replace("%data%", work.jobs[0].title);
-
-//var Intbutton = HTMLheaderName.replace("%data%", GintName);
-
-//$("#header").prepend(Intbutton);
-$("#header").prepend(formattedRole);
-$("#header").prepend(formattedName);
 
 
-
-
-//$("#main").append(bio.contacts);
-
-//function inName(name) {
-   // name = bio.name.split(" ");
-  //  console.log(name);
-  //  name[1] = name[1].toUpperCase();
- //   name[0] = name[0].slice(0,1).toUpperCase() + name[0].slice(1).toLowerCase();
-
- //   return name[0] +[" "]+name[1];
-//   }
-
-//$("#main").append(internationalizeButton);
